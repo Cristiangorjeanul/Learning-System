804ab666da4c5f3665a2ec0d922b913a653ab3f2
Cristian T <Cristian.gorjeanul@gmail.com>
2020-10-28 21:07:15 +0200

    Add html, Css, JavaScript-jQuery files

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(function () {
+
+  var category = $('.category');
+  category.find('dd').hide();
+
+  function blinkNews() {
+    $('.blink').fadeOut(1000);
+    $('.blink').fadeIn(1000);
+  }
+  setInterval(blinkNews, 2500);
+
+  category.find('dt').on('click', function () {
+    // toggle hide an element if is visible
+    // or display the element if is not visible
+    $(this).next().toggle();
+  });
+
+
+
+});
